220822592fc150601a808811a99a5907998cd3f1
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-15 21:07:12 +0100

    Upgrade Pari e Dispari

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -1,55 +1,54 @@
-// Chiediamo all'utente di scegliere un numero da "1" a "5".
+// Chiediamo all' utente di scegliere un numero da "1" a "5".
 
-let userNumber = parseInt(prompt("Scegli un numero da '1' a '5'"));
-
-console.log("Questo è il mio numero", userNumber);
+let userNumber = parseInt(prompt("Inserisci un numero da '1' a '5' "));
+console.log("Il mio numero è", userNumber);
 
 // Chiediamo all'utente di scegliere "pari" o "dispari".
 
 let userScelta = prompt("Scegli pari o dispari");
+console.log( "La mia scelta è:",userScelta);
 
-console.log("Questa è la mia scelta:", userScelta);
 
 // Andiamo a creare una variabile per il "Pc".
 
-let randomPC = numeroRandom();
+let randomPc = numeroRandom();
+console.log("Il numero generato per il pc è: ",randomPc);
 
-console.log("Il numero generato per il Pc è:", randomPC);
 
-//Andiamo a creare una funzione per generare un numero "random" per il pc.
+// Andiamo a creare una funzione per generare un numero "random per il pc".
 
-function numeroRandom(numeroPc){
+function numeroRandom(){
 
     let numberPc = Math.floor(Math.random() * 5) + 1;
 
     return numberPc;
 }
 
-// Andiamo a sommare lo userNumber + randomPc.
+// Andiamo a sommare l'"userNumber + randomPc".
 
-let somma = userNumber + randomPC;
-
-console.log("La somma dei due numeri è: ", somma);
+let somma = userNumber + randomPc;
+console.log("la somma totale è:",somma);
 
 // Andiamo a creare la nostra variabile "risultato".
 
-let risultato = pariDispari(somma);
-console.log(risultato);
+let risultato = pariDispari();
+console.log("Il risultato è: ", risultato);
 
-// Andiamo a creare la funzione se la somma sarà pari o dispari.
+// Andiamo a creare la funzione se la somma sarà pari o pariDispari.
 
 function pariDispari(){
     if(somma % 2 === 0){
-        return "La somma è pari";
-    }else{ 
-        return "La somma è dispari";
+        return "pari";
+    }else{
+        return "dispari";
     }
 }
 
-// Andiamo a creare una variabile per decretare il vincitore.
+// Andiamo a creare una condizione per decretare il vincitore.
 
 if(userScelta === risultato){
-    console.log("Hai Vinto");
+    document.getElementById("output").innerHTML = ` Complimenti, hai vinto!!!
+    Hai scelto il numero: "${userNumber}", la tua scelta è: "${userScelta}" e la somma totale è: "${somma}"`;
 }else{
-    console.log("Mi dispiace hai perso!");
+    document.getElementById("output").innerHTML = "Mi dispiace, Hai perso!";
 }
